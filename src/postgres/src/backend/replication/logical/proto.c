c1e3eaecb7839fea8dcec6036d22139fa1054dd0
Saurav Tiwary <21277837+dr0pdb@users.noreply.github.com>
2024-02-06 18:00:01 +0530

    Introduce support for update operation

--- src/postgres/src/backend/replication/logical/proto.c
+++ src/postgres/src/backend/replication/logical/proto.c
@@ -16,6 +16,7 @@
 #include "catalog/pg_namespace.h"
 #include "catalog/pg_type.h"
 #include "libpq/pqformat.h"
+#include "pg_yb_utils.h"
 #include "replication/logicalproto.h"
 #include "utils/builtins.h"
 #include "utils/lsyscache.h"
@@ -31,7 +32,7 @@
 
 static void logicalrep_write_attrs(StringInfo out, Relation rel);
 static void logicalrep_write_tuple(StringInfo out, Relation rel,
-					   HeapTuple tuple);
+					   HeapTuple tuple, bool *yb_is_omitted);
 
 static void logicalrep_read_attrs(StringInfo in, LogicalRepRelation *rel);
 static void logicalrep_read_tuple(StringInfo in, LogicalRepTupleData *tuple);
@@ -151,7 +152,7 @@ logicalrep_write_insert(StringInfo out, Relation rel, HeapTuple newtuple)
 	pq_sendint32(out, RelationGetRelid(rel));
 
 	pq_sendbyte(out, 'N');		/* new tuple follows */
-	logicalrep_write_tuple(out, rel, newtuple);
+	logicalrep_write_tuple(out, rel, newtuple, NULL);
 }
 
 /*
@@ -183,7 +184,8 @@ logicalrep_read_insert(StringInfo in, LogicalRepTupleData *newtup)
  */
 void
 logicalrep_write_update(StringInfo out, Relation rel, HeapTuple oldtuple,
-						HeapTuple newtuple)
+						HeapTuple newtuple, bool *yb_old_is_omitted,
+						bool *yb_new_is_omitted)
 {
 	pq_sendbyte(out, 'U');		/* action UPDATE */
 
@@ -200,11 +202,11 @@ logicalrep_write_update(StringInfo out, Relation rel, HeapTuple oldtuple,
 			pq_sendbyte(out, 'O');	/* old tuple follows */
 		else
 			pq_sendbyte(out, 'K');	/* old key follows */
-		logicalrep_write_tuple(out, rel, oldtuple);
+		logicalrep_write_tuple(out, rel, oldtuple, yb_old_is_omitted);
 	}
 
 	pq_sendbyte(out, 'N');		/* new tuple follows */
-	logicalrep_write_tuple(out, rel, newtuple);
+	logicalrep_write_tuple(out, rel, newtuple, yb_new_is_omitted);
 }
 
 /*
@@ -268,7 +270,7 @@ logicalrep_write_delete(StringInfo out, Relation rel, HeapTuple oldtuple)
 	else
 		pq_sendbyte(out, 'K');	/* old key follows */
 
-	logicalrep_write_tuple(out, rel, oldtuple);
+	logicalrep_write_tuple(out, rel, oldtuple, NULL);
 }
 
 /*
@@ -441,7 +443,8 @@ logicalrep_read_typ(StringInfo in, LogicalRepTyp *ltyp)
  * Write a tuple to the outputstream, in the most efficient format possible.
  */
 static void
-logicalrep_write_tuple(StringInfo out, Relation rel, HeapTuple tuple)
+logicalrep_write_tuple(StringInfo out, Relation rel, HeapTuple tuple,
+					   bool *yb_is_omitted)
 {
 	TupleDesc	desc;
 	Datum		values[MaxTupleAttributeNumber];
@@ -476,6 +479,16 @@ logicalrep_write_tuple(StringInfo out, Relation rel, HeapTuple tuple)
 		/* skip dropped columns */
 		if (att->attisdropped)
 			continue;
+
+		if (IsYugaByteEnabled() && yb_is_omitted && yb_is_omitted[i])
+		{
+			/*
+			 * Treat omitted column as an unchanged toast column so that the
+			 * client will ignore it.
+			 */
+			pq_sendbyte(out, 'u'); /* unchanged toast column */
+			continue;
+		}
 
 		if (isnull[i])
 		{
--- src/postgres/src/backend/replication/logical/yb_decode.c
+++ src/postgres/src/backend/replication/logical/yb_decode.c
@@ -33,7 +33,12 @@
 static void
 YBDecodeInsert(LogicalDecodingContext *ctx, XLogReaderState *record);
 static void
+YBDecodeUpdate(LogicalDecodingContext *ctx, XLogReaderState *record);
+static void
 YBDecodeCommit(LogicalDecodingContext *ctx, XLogReaderState *record);
+
+static int
+YBFindAttributeIndexInDescriptor(TupleDesc tupdesc, const char *column_name);
 
 void
 YBLogicalDecodingProcessRecord(LogicalDecodingContext *ctx,
@@ -42,12 +47,13 @@ YBLogicalDecodingProcessRecord(LogicalDecodingContext *ctx,
 	elog(DEBUG4,
 		 "YBLogicalDecodingProcessRecord: Decoding record with action = %d.",
 		 record->yb_virtual_wal_record->data->action);
+	YBC_LOG_INFO("YBLogicalDecodingProcessRecord - %u", record->yb_virtual_wal_record->data->action);
 	switch (record->yb_virtual_wal_record->data->action)
 	{
 		case YB_PG_ROW_MESSAGE_ACTION_UNKNOWN: switch_fallthrough();
 		case YB_PG_ROW_MESSAGE_ACTION_DDL:
 			/* Ignore UNKNOWN/DDL records. */
-			return;
+			break;
 
 		case YB_PG_ROW_MESSAGE_ACTION_BEGIN:
 			/*
@@ -56,7 +62,7 @@ YBLogicalDecodingProcessRecord(LogicalDecodingContext *ctx,
 			 * after processing the corresponding commit record.
 			 */
 			StartTransactionCommand();
-			return;
+			break;
 
 		case YB_PG_ROW_MESSAGE_ACTION_INSERT:
 		{
@@ -64,8 +70,14 @@ YBLogicalDecodingProcessRecord(LogicalDecodingContext *ctx,
 			break;
 		}
 
-		/* TODO(#20726): Support Update and Delete operations. */
-		case YB_PG_ROW_MESSAGE_ACTION_UPDATE: switch_fallthrough();
+		case YB_PG_ROW_MESSAGE_ACTION_UPDATE:
+		{
+			YBC_LOG_INFO("Update action");
+			YBDecodeUpdate(ctx, record);
+			break;
+		}
+
+		/* TODO(#20726): Support Delete operations. */
 		case YB_PG_ROW_MESSAGE_ACTION_DELETE:
 			break;
 
@@ -122,31 +134,132 @@ YBDecodeInsert(LogicalDecodingContext *ctx, XLogReaderState *record)
 	for (int col_idx = 0; col_idx < yb_record->data->col_count; col_idx++)
 	{
 		YBCPgDatumMessage *col = &yb_record->data->cols[col_idx];
-		int attr_idx = 0;
-		for (attr_idx = 0; attr_idx < nattrs; attr_idx++)
-		{
-			if (!strcmp(tupdesc->attrs[attr_idx].attname.data, col->column_name))
-				break;
-		}
-		if (attr_idx == nattrs)
-		{
-			ereport(ERROR,
-					(errcode(ERRCODE_INTERNAL_ERROR),
-					 errmsg("Could not find column with name %s"
-							" in tuple descriptor",
-							col->column_name)));
-			continue;
-		}
 
-		datums[attr_idx] = col->datum;
-		is_nulls[attr_idx] = col->is_null;
+		/*
+		 * Column name is null when both new and old values are omitted. This
+		 * can only happen in the case of Delete operations.
+		 */
+		Assert(col->column_name);
+
+		/*
+		 * We should always receive all the new columns as part of the Insert
+		 * operation.
+		 */
+		Assert(!col->new_is_omitted);
+
+		int attr_idx =
+			YBFindAttributeIndexInDescriptor(tupdesc, col->column_name);
+		datums[attr_idx] = col->new_datum;
+		is_nulls[attr_idx] = col->new_is_null;
 	}
 	tuple = heap_form_tuple(tupdesc, datums, is_nulls);
 
-	tuple_buf = ReorderBufferGetTupleBuf(ctx->reorder, tuple->t_len + HEAPTUPLESIZE);
+	tuple_buf =
+		ReorderBufferGetTupleBuf(ctx->reorder, tuple->t_len + HEAPTUPLESIZE);
 	tuple_buf->tuple = *tuple;
 	change->data.tp.newtuple = tuple_buf;
 	change->data.tp.oldtuple = NULL;
+	change->data.tp.yb_table_oid = yb_record->table_oid;
+
+	change->data.tp.clear_toast_afterwards = true;
+	ReorderBufferQueueChange(ctx->reorder, yb_record->xid,
+							 ctx->reader->ReadRecPtr, change);
+
+	RelationClose(relation);
+}
+
+/*
+ * YB version of the DecodeUpdate function from decode.c
+ */
+static void
+YBDecodeUpdate(LogicalDecodingContext *ctx, XLogReaderState *record)
+{
+	YBCPgVirtualWalRecord	*yb_record = record->yb_virtual_wal_record;
+	ReorderBufferChange		*change = ReorderBufferGetChange(ctx->reorder);
+	Relation				relation;
+	TupleDesc				tupdesc;
+	int						nattrs;
+	HeapTuple				new_tuple;
+	HeapTuple				old_tuple;
+	ReorderBufferTupleBuf	*new_tuple_buf;
+	ReorderBufferTupleBuf	*old_tuple_buf = NULL;
+	bool					*old_is_omitted = NULL;
+	bool					*new_is_omitted = NULL;
+
+	YBC_LOG_INFO("YBDecodeUpdate");
+
+	change->action = REORDER_BUFFER_CHANGE_UPDATE;
+	change->lsn = yb_record->lsn;
+	change->origin_id = yb_record->lsn;
+
+	/*
+	 * TODO(#20726): This is the schema of the relation at the streaming time.
+	 * We need this to be the schema of the table at record commit time.
+	 */
+	relation = RelationIdGetRelation(yb_record->table_oid);
+	if (!RelationIsValid(relation))
+		elog(ERROR, "could not open relation with OID %u",
+			 yb_record->table_oid);
+
+	tupdesc = RelationGetDescr(relation);
+	nattrs = tupdesc->natts;
+
+	/*
+	 * Allocate is_omitted arrays before so that we can directly write to it
+	 * instead of creating a temporary array and doing a memcpy.
+	 * Assume that columns are omitted by default.
+	 */
+	old_is_omitted = YBAllocateIsOmittedArray(ctx->reorder, nattrs);
+	new_is_omitted = YBAllocateIsOmittedArray(ctx->reorder, nattrs);
+	memset(new_is_omitted, 1, sizeof(bool) * nattrs);
+	memset(old_is_omitted, 1, sizeof(bool) * nattrs);
+
+	Datum new_datums[nattrs];
+	bool new_is_nulls[nattrs];
+	Datum old_datums[nattrs];
+	bool old_is_nulls[nattrs];
+	memset(new_is_nulls, 1, sizeof(new_is_nulls));
+	memset(old_is_nulls, 1, sizeof(old_is_nulls));
+	for (int col_idx = 0; col_idx < yb_record->data->col_count; col_idx++)
+	{
+		YBCPgDatumMessage *col = &yb_record->data->cols[col_idx];
+
+		/*
+		 * Column name is null when both new and old values are omitted. This
+		 * can only happen in the case of Delete operations.
+		 */
+		Assert(col->column_name);
+
+		int attr_idx =
+			YBFindAttributeIndexInDescriptor(tupdesc, col->column_name);
+
+		if (!col->new_is_omitted)
+		{
+			new_datums[attr_idx] = col->new_datum;
+			new_is_nulls[attr_idx] = col->new_is_null;
+			new_is_omitted[attr_idx] = false;
+		}
+
+		if (!col->old_is_omitted)
+		{
+			old_datums[attr_idx] = col->old_datum;
+			old_is_nulls[attr_idx] = col->old_is_null;
+			old_is_omitted[attr_idx] = false;
+		}
+	}
+
+	new_tuple = heap_form_tuple(tupdesc, new_datums, new_is_nulls);
+	new_tuple_buf = ReorderBufferGetTupleBuf(ctx->reorder, new_tuple->t_len + HEAPTUPLESIZE);
+	new_tuple_buf->tuple = *new_tuple;
+	new_tuple_buf->yb_is_omitted = new_is_omitted;
+
+	old_tuple = heap_form_tuple(tupdesc, old_datums, old_is_nulls);
+	old_tuple_buf = ReorderBufferGetTupleBuf(ctx->reorder, old_tuple->t_len + HEAPTUPLESIZE);
+	old_tuple_buf->tuple = *old_tuple;
+	old_tuple_buf->yb_is_omitted = old_is_omitted;
+
+	change->data.tp.newtuple = new_tuple_buf;
+	change->data.tp.oldtuple = old_tuple_buf;
 	change->data.tp.yb_table_oid = yb_record->table_oid;
 
 	change->data.tp.clear_toast_afterwards = true;
@@ -175,4 +288,29 @@ YBDecodeCommit(LogicalDecodingContext *ctx, XLogReaderState *record)
 	ReorderBufferCommit(ctx->reorder, yb_record->xid, commit_lsn,
 						end_lsn, yb_record->data->commit_time, origin_id,
 						origin_lsn);
+}
+
+/*
+ * TODO(#20726): Optimize this lookup via a cache. We do not need to iterate
+ * through all attributes everytime this function is called.
+ */
+static int
+YBFindAttributeIndexInDescriptor(TupleDesc tupdesc, const char *column_name)
+{
+	int attr_idx = 0;
+	for (attr_idx = 0; attr_idx < tupdesc->natts; attr_idx++)
+	{
+		if (!strcmp(tupdesc->attrs[attr_idx].attname.data, column_name))
+			break;
+	}
+	if (attr_idx == tupdesc->natts)
+	{
+		ereport(ERROR,
+				(errcode(ERRCODE_INTERNAL_ERROR),
+					errmsg("Could not find column with name %s in tuple"
+						   " descriptor", column_name)));
+		return -1;
+	}
+
+	return attr_idx;
 }
